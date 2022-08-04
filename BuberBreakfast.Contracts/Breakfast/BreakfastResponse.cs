7d250f8ccbe2fb771d999b405bb913b76c2d5fb2
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-08-04 20:47:39 +0430

    add breakfast models in Breakfast.Contract

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace BuberBreakfast.Contracts.Breakfast;
+
+public record CreateBreakfastResponse(
+    Guid Id,
+    string Name,
+    string Description,
+    DateTime StarDateTime,
+    DateTime EndDateTime,
+    DateTime LastModifiedDateTime,
+    List<string> Savory,
+    List<string> Sweet);
